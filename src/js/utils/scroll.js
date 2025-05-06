35e557e71038d04e9db55485445552ed418fea8c
Kulov_Olga <kulov_olga@dev-profinyanya.ru>
2025-05-06 12:16:58 +0400

    fix scroll btn

--- src/js/utils/scroll.js
+++ src/js/utils/scroll.js
@@ -11,6 +11,20 @@
         });
       });
     }
+    const anchorsEnd = document.querySelectorAll(`.nav-link-end`);
+  
+
+
+    for (let anchor of anchorsEnd) {
+      anchor.addEventListener('click', (e) => {
+        e.preventDefault();
+        const blockId = anchor.getAttribute('href');
+        document.querySelector('' + blockId).scrollIntoView({
+          behavior: 'smooth',
+          block: 'end'
+        });
+      });
+    }
   }
 
   export default scroll 
